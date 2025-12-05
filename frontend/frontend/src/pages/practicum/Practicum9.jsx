6854f992776c2a67caf061f13bb83f3cbf728fd0
Nikko Paceno <npaceno@sixb.local>
2025-12-05 14:48:22 -0500

    commit

--- frontend/frontend/src/pages/practicum/Practicum9.jsx
+++ frontend/frontend/src/pages/practicum/Practicum9.jsx
@@ -1,24 +1,12 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 const Practicum9 = () => {
-  const students = [
-    { name: "Alice", score: 85 },
-    { name: "Bob", score: 92 },
-    { name: "Charlie", score: 68 },
-    { name: "David", score: 74 },
-    { name: "Eve", score: 91 }
-  ];
+  
 
+  
   return (
     <>
-      <h2>Student Scores</h2>
-      <ul>
-        {students.map((student, index) => (
-          <li key={index}>
-            {student.name}: {student.score}
-          </li>
-        ))}
-      </ul>
+   
     </>
   );
 };
